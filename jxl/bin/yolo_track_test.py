67b4ae18a0380069bf898e2ee84283f1b26376f6
dayn9t <dayn9t@gmail.com>
2025-04-12 17:40:16 +0800

    25-04-12

--- jxl/bin/yolo_track_test.py
+++ jxl/bin/yolo_track_test.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from time import sleep, time
 from typing import Final
 
-import cv2  # type: ignore
+import cv2
 from jcx.text.txt_json import to_json, load_json
 from js4.task import TaskDb, D2dParams
 from loguru import logger
