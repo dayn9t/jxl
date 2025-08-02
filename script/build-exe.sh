400b9d51185188e9488438d8c8532cf928fae94e
dayn9t <dayn9t@gmail.com>
2025-08-02 20:47:19 +0800

    25-08-02

--- script/build-exe.sh
+++ script/build-exe.sh
@@ -5,7 +5,16 @@ ROOT=$(realpath "$script_dir/..")
 BIN_DIR=$ROOT/src/jxl/bin
 DST_DIR=$HOME/.local/bin
 
-scrips="jxl_label.py jxl_label_clean.py jxl_prop.py jxl_split.py jxl_sample.py jxl_viewer.py yolo_detect.py"
+scrips="
+  jxl_label.py
+  jxl_label_clean.py
+  jxl_prop.py
+  jxl_split.py
+  jxl_sample.py
+  jxl_viewer.py
+  yolo_detect.py
+  d2d_label.py
+"
 for script in $scrips; do
     uv run nuitka --onefile --standalone --output-dir=dist "$BIN_DIR/$script"
 done
